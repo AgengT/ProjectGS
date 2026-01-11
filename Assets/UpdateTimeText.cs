08b04d5773ebdefdd55c203b13d4753d153442bf
AgengT <agengtawang01@gmail.com>
2026-01-12 03:22:07 +0700

    Add: Finish room

---
+++ Assets/UpdateTimeText.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UpdateTimeText : MonoBehaviour
+{
+   [SerializeField] private TMPro.TextMeshProUGUI timeText;
+
+   public void UpdateTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60F);
+        int seconds = Mathf.FloorToInt(time - minutes * 60);
+        timeText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
+}
