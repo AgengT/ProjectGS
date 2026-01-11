06e51f501ca9fcb1d93720255471c048bbd708fc
AgengT <agengtawang01@gmail.com>
2026-01-11 11:07:24 +0700

    Add: Pause UI & UI Manager
    Add TextMeshPro resources

---
+++ Assets/[Game System]/Core Managers/UIManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject pauseMenu;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        ShowMainMenu();
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void EnterGame()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+
+    public void ShowPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+}
